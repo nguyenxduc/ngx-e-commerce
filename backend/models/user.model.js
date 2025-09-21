e61e9a5f6a26a3bd99af10af0c052998f365d3f6
nguyenxduc <ncd270705@gmail.com>
2025-09-21 09:15:51 +0700

    delete non-neccessory file

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema(
 
     role: {
       type: String,
-      enum: ["customer", "admin", "seller"],
+      enum: ["customer", "admin"],
       default: "customer",
     },
     shop: {
